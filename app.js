7cd4e201b9970e65857a9d3c49904f6fa6ededa9
Thossaporn saisud <59160761@go.buu.ac.th>
2019-08-17 10:15:14 +0700

    Week 4

--- app.js
+++ app.js
@@ -4,19 +4,38 @@ const port = 3000
 
 app.use(express.json())
 
+class Pokemons {
+    constructor(name, type) {
+        this.name = name
+        this.type = type
+        this.id = null
+    }
+}
+let pokemons = []
+
+function createPokemon(name, type) {
+    let p = new Pokemons(name, type)
+    p.id = generateNewId(pokemons.length)
+    return p
+}
+
+function generateNewId(num) {
+    return num + 1
+}
+
+pokemons.push(createPokemon('Wartortle', 'Water'))
+pokemons.push(createPokemon('Butterfree', 'Bug, Flying'))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/pokemon', function (req, res) {
     res.send(pokemons)
 })
 app.post('/pokemons', (req, res) => {
-    pokemons.push(req.body)
+    let p = new Pokemons(req.body.name, req.body.type)
+    p.id = generateNewId(pokemons.length)
+    pokemons.push(p)
     res.sendStatus(201)
 })
-
-let pokemons = [
-    { name: 'Pikachu', type: 'Electic' },
-    { name: 'Pikachu', type: 'Fire' }
-]
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
